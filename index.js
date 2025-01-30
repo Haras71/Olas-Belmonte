cffb7d1563fdd4f7897cfebd6c4e06b5198135b0
Haras71 <soares.hara@gmail.com>
2025-01-30 11:50:28 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+{
+  "name": "my-nodejs-app",
+  "version": "1.0.0",
+  "description": "My Node.js Application",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js",
+    "dev": "nodemon index.js"
+  },
+  "dependencies": {
+    // Your dependencies here
+  }
+}
